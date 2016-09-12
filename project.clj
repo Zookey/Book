447aff2aabaf81f276fccf6e6fe7f759fa986ae3
Zoran Pavlovic <zoranp90@gmail.com>
2016-09-12 14:52:22 +0200

    Added required depedencies.

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
-(defproject bookapp "0.1.0-SNAPSHOT"
+(defproject book "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hiccup "1.0.5"] 
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [mysql/mysql-connector-java "5.1.38"]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]]
+  
+  :plugins [[lein2-eclipse "2.0.0"]
+            [lein-ring "0.8.10"]]
+  
+  :ring {:handler book.handler/app})
