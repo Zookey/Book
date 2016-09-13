2b2a6f0a254c05407befd65cc8fb0739e666b0f5
Zoran Pavlovic <zoranp90@gmail.com>
2016-09-13 14:46:21 +0200

    Styling view with Bootstrap

--- src/bookapp/view.clj
+++ src/bookapp/view.clj
@@ -9,9 +9,10 @@
 
 
 (defn display-all-books [books]
-  [:div 
+  [:div {:class "card text-center"}
    [:h1 "Show all books"]
    [:p "Latest books from our store"]
+   [:br]
    [:table  {:class "table table-bordered"}
     [:th "id"]
     [:th "title"]
@@ -33,32 +34,32 @@
          ]) books)]])
 
 (defn add-book-form []
-  [:div {:class "form-group"} 
-   [:h1 "Add book"]
+  [:div {:class "form-group card"} 
+   [:h1 {:class "text-center"} "Add book"]
    (form/form-to [:post "/"]
                  (anti-forgery/anti-forgery-field)
                  [:div {:class "form-group"}
-                  (form/label "title" "Title: ")
+                  (form/label "title" "Title ")
                   (form/text-field {:class "form-control"} "title")]
                  [:div {:class "form-group"}                  
-                  (form/label "description" "Description: ")                  
+                  (form/label "description" "Description ")                  
                   (form/text-field {:class "form-control"}  "description")]
                  [:div {:class "form-group"}                  
-                  (form/label "isbn" "ISBN: ")                  
+                  (form/label "isbn" "ISBN ")                  
                   (form/text-field {:class "form-control"}  "isbn")]
                  [:div {:class "form-group"}                  
-                  (form/label "author" "Author: ")                  
+                  (form/label "author" "Author ")                  
                   (form/text-field {:class "form-control"}  "author")]
                  (form/submit-button {:class "btn btn-primary btn-lg btn-block"}  "Add book")
                  [:br])])
 
 (defn index-page [books] 
   (layout/common-layout ""
-                        [:div {:class "col-lg-2"}]
-                        [:div {:class "col-lg-8"}
+                        [:div {:class "col-lg-1"}]
+                        [:div {:class "col-lg-10"}
                          (display-all-books books)                        
                          (add-book-form)]
-                        [:div {:class "col-lg-2"}]   
+                        [:div {:class "col-lg-1"}]   
                         ))
 
 
