256c69e83a7131c4d1a096137cdbe3596a9be286
Zoran Pavlovic <zoranp90@gmail.com>
2016-09-13 12:05:18 +0200

    Styling table for all books and add book form.

--- src/bookapp/view.clj
+++ src/bookapp/view.clj
@@ -10,53 +10,55 @@
 
 (defn display-all-books [books]
   [:div 
-   [:table 
+   [:h1 "Show all books"]
+   [:p "Latest books from our store"]
+   [:table  {:class "table table-bordered"}
     [:th "id"]
     [:th "title"]
     [:th "description"]
     [:th "isbn"]
     [:th "author"]
-     [:th "delete"]
-     [:th "update"]
-     (map 
-       (fn [book]
-         [:tr 
-          [:td (h (:id book))]
-          [:td (h (:title book))]
-          [:td (h (:description book))]
-          [:td (h (:isbn book))]
-          [:td (h (:author book))]
-          [:td [:a {:href (str "/delete/" (h (:id book)))} "delete"]]
-          [:td [:a {:href (str "/update/" (h (:id book)))} "update"]]
-          ]) books)]])
+    [:th "delete"]
+    [:th "update"]
+    (map 
+      (fn [book]
+        [:tr 
+         [:td (h (:id book))]
+         [:td (h (:title book))]
+         [:td (h (:description book))]
+         [:td (h (:isbn book))]
+         [:td (h (:author book))]
+         [:td [:a {:href (str "/delete/" (h (:id book)))} "delete"]]
+         [:td [:a {:href (str "/update/" (h (:id book)))} "update"]]
+         ]) books)]])
 
 (defn add-book-form []
-  [:div (form/form-to [:post "/"]
-                      (anti-forgery/anti-forgery-field)
-                      (form/label "title" "Title: ")
-                      [:br]
-                      (form/text-field "title")
-                      [:br]
-                      (form/label "description" "Description: ")
-                      [:br]
-                      (form/text-field "description")
-                      [:br]
-                      (form/label "isbn" "ISBN: ")
-                      [:br]
-                      (form/text-field "isbn")
-                      [:br]
-                      (form/label "author" "Author: ")
-                      [:br]
-                      (form/text-field "author")
-                      [:br]
-                      [:br]
-                      (form/submit-button {:class "btn btn-primary"}  "Add book")
-                      [:br])])
+  [:div {:class "form-group"} 
+   [:h1 "Add book"]
+   (form/form-to [:post "/"]
+                 (anti-forgery/anti-forgery-field)
+                 [:div {:class "form-group"}
+                  (form/label "title" "Title: ")
+                  (form/text-field {:class "form-control"} "title")]
+                 [:div {:class "form-group"}                  
+                  (form/label "description" "Description: ")                  
+                  (form/text-field {:class "form-control"}  "description")]
+                 [:div {:class "form-group"}                  
+                  (form/label "isbn" "ISBN: ")                  
+                  (form/text-field {:class "form-control"}  "isbn")]
+                 [:div {:class "form-group"}                  
+                  (form/label "author" "Author: ")                  
+                  (form/text-field {:class "form-control"}  "author")]
+                 (form/submit-button {:class "btn btn-primary btn-lg btn-block"}  "Add book")
+                 [:br])])
 
 (defn index-page [books] 
-  (layout/common-layout "Books"
-                        (display-all-books books)                        
-                        (add-book-form)
+  (layout/common-layout ""
+                        [:div {:class "col-lg-2"}]
+                        [:div {:class "col-lg-8"}
+                         (display-all-books books)                        
+                         (add-book-form)]
+                        [:div {:class "col-lg-2"}]   
                         ))
 
 
